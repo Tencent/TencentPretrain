bc593c055dfcd376c7eee07e57dbbc3d2083683a
P01son6415 <liyudong123@hotmail.com>
2022-12-05 17:01:49 +0800

    update

--- tencentpretrain/model_loader.py
+++ tencentpretrain/model_loader.py
@@ -6,7 +6,7 @@ def load_model(model, model_path):
     Load model from saved weights.
     """
     if hasattr(model, "module"):
-        model.module.load_state_dict(torch.load(model_path, map_location="cpu"), strict=True)
+        model.module.load_state_dict(torch.load(model_path, map_location="cpu"), strict=False)
     else:
-        model.load_state_dict(torch.load(model_path, map_location="cpu"), strict=True)
+        model.load_state_dict(torch.load(model_path, map_location="cpu"), strict=False)
     return model
