5f6670218487117fd4ca54fbe82970ef2c3625b9
P01son6415 <liyudong123@hotmail.com>
2023-03-09 10:39:27 +0800

    add RSMNorm

--- tencentpretrain/layers/layer_norm.py
+++ tencentpretrain/layers/layer_norm.py
@@ -37,3 +37,17 @@ class T5LayerNorm(nn.Module):
         hidden_states = hidden_states * torch.rsqrt(variance + self.variance_epsilon)
 
         return self.weight * hidden_states.type_as(self.weight)
+
+
+class RMSNorm(torch.nn.Module):
+    def __init__(self, hidden_size, eps=1e-6):
+        super().__init__()
+        self.eps = eps
+        self.weight = nn.Parameter(torch.ones(hidden_size))
+
+    def _norm(self, x):
+        return x * torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps)
+
+    def forward(self, x):
+        output = self._norm(x.float()).type_as(x)
+        return output * self.weight
