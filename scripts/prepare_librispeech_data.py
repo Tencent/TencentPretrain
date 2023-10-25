b2430e727f739a1c563c58925392f58ab49071a3
janinezhao <janinezhao@tencent.com>
2023-10-25 17:28:50 +0800

    fix prepare

--- scripts/prepare_librispeech_data.py
+++ scripts/prepare_librispeech_data.py
@@ -12,10 +12,10 @@ import random
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("root")
-    parser.add_argument("--output-file", required=True)
-    parser.add_argument("--for_finetune", action='store_true')
-    parser.add_argument("--sample-ratio", type=float, default=1.0)
+    parser.add_argument("--input_path", required=True)
+    parser.add_argument("--output_file", required=True)
+    parser.add_argument("--add_headline", action='store_true')
+    parser.add_argument("--sample_ratio", type=float, default=1.0)
     parser.add_argument("--ext", default="flac")
     parser.add_argument("--seed", type=int, default=7)
     args = parser.parse_args()
@@ -23,13 +23,13 @@ def main():
     rand = random.Random(args.seed)
     os.makedirs(os.path.dirname(os.path.realpath(args.output_file)), exist_ok=True)
 
-    dir_path = os.path.realpath(args.root)
+    dir_path = os.path.realpath(args.input_path)
     search_path = os.path.join(dir_path, "**/*." + args.ext)
 
     transcriptions = {}
 
     with open(args.output_file, "w") as out_file:
-        if args.for_finetune:
+        if args.add_headline:
             print("text" + "\t" + "wav_path", file=out_file)
         for fname in glob.iglob(search_path, recursive=True):
             if rand.random() > args.sample_ratio:
@@ -40,7 +40,7 @@ def main():
             if dir not in transcriptions:
                 parts = dir.split(os.path.sep)
                 trans_path = f"{parts[-2]}-{parts[-1]}.trans.txt"
-                path = os.path.join(args.root, dir, trans_path)
+                path = os.path.join(args.input_path, dir, trans_path)
                 assert os.path.exists(path)
                 texts = {}
                 with open(path, "r") as trans_f:
