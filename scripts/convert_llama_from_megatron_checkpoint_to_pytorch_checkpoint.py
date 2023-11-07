2244be8034dc52fc23265d79490ddd51c43af7fc
karots123 <962813115@qq.com>
2023-11-07 15:09:57 +0800

    Add files via upload

---
+++ scripts/convert_llama_from_megatron_checkpoint_to_pytorch_checkpoint.py
@@ -0,0 +1,57 @@
+import argparse
+import os 
+import torch
+
+
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("--input_model_path", type=str, default="models/input_model",
+                        help=".")
+parser.add_argument("--output_model_path", type=str, default="models/output_model",
+                        help=".")
+parser.add_argument("--layers_num", type=int, default=32)
+parser.add_argument("--tp_size", type=int, default=4)
+
+
+args = parser.parse_args()
+
+if not os.path.exists(args.output_model_path):
+    print('创建路径',args.output_model_path)
+    os.mkdir(args.output_model_path)
+
+output_model=torch.load(os.path.join(args.input_model_path,'mp_rank_00_model_states.pt'),map_location='cpu')["module"]
+
+for n in range(1,args.tp_size):
+    index=str(n) if len(str(n))==2 else '0'+str(n)
+    model_name=f"mp_rank_{index}_model_states.pt"
+    model_piece = torch.load(os.path.join(args.input_model_path,model_name),map_location="cpu")["module"]
+    output_model["embedding.word.embedding.weight"] = torch.cat((output_model["embedding.word.embedding.weight"],model_piece["embedding.word.embedding.weight"]),dim=-2)
+    
+    for i in range(args.layers_num):
+        for j in range(3):
+            tensor_a=output_model["encoder.transformer." + str(i) + ".self_attn.linear_layers."+ str(j) +".weight"]
+            tensor_b=model_piece["encoder.transformer." + str(i) + ".self_attn.linear_layers."+ str(j) +".weight"]
+            output_model["encoder.transformer." + str(i) + ".self_attn.linear_layers."+ str(j) +".weight"]=torch.cat((tensor_a,tensor_b),dim=-2)
+        
+        tensor_a=output_model["encoder.transformer." + str(i) + ".self_attn.final_linear.weight"]
+        tensor_b=model_piece["encoder.transformer." + str(i) + ".self_attn.final_linear.weight"]
+      
+        output_model["encoder.transformer." + str(i) + ".self_attn.final_linear.weight"]=torch.cat((tensor_a,tensor_b),dim=-1)
+        
+        tensor_a=output_model["encoder.transformer." + str(i) + ".feed_forward.linear_1.weight"]
+        tensor_b=model_piece["encoder.transformer." + str(i) + ".feed_forward.linear_1.weight"]
+        output_model["encoder.transformer." + str(i) + ".feed_forward.linear_1.weight"]=torch.cat((tensor_a,tensor_b),dim=-2)
+        
+        tensor_a=output_model["encoder.transformer." + str(i) + ".feed_forward.linear_gate.weight"]
+        tensor_b=model_piece["encoder.transformer." + str(i) + ".feed_forward.linear_gate.weight"]
+        output_model["encoder.transformer." + str(i) + ".feed_forward.linear_gate.weight"]=torch.cat((tensor_a,tensor_b),dim=-2)
+                
+        tensor_a=output_model["encoder.transformer." + str(i) + ".feed_forward.linear_2.weight"]
+        tensor_b=model_piece["encoder.transformer." + str(i) + ".feed_forward.linear_2.weight"]
+        output_model["encoder.transformer." + str(i) + ".feed_forward.linear_2.weight"]=torch.cat((tensor_a,tensor_b),dim=-1)
+    
+    tensor_a=output_model["target.lm.output_layer.weight"]
+    tensor_b=model_piece["target.lm.output_layer.weight"]
+    output_model["target.lm.output_layer.weight"]=torch.cat((tensor_a,tensor_b),dim=-2)
+
+torch.save(output_model,os.path.join(args.output_model_path,'merge_model.bin'))
+ 
---
+++ scripts/convert_llama_from_pytorch_checkpoint_to_megatron_checkpoint.py
@@ -0,0 +1,62 @@
+import argparse
+import collections
+import torch
+import os 
+
+
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("--input_model_path", type=str, default="models/input_model.bin",
+                        help=".")
+parser.add_argument("--output_model_path", type=str, default="models/output_model",
+                        help=".")
+parser.add_argument("--layers_num", type=int, default=32)
+parser.add_argument("--tp_size", type=int, default=4)
+parser.add_argument("--hidden_size", type=int, default=4096)
+parser.add_argument("--feedforward_size", type=int, default=11008)
+
+args = parser.parse_args()
+
+input_model = torch.load(args.input_model_path)
+
+if not os.path.exists(args.output_model_path):
+    print('创建路径',args.output_model_path)
+    os.mkdir(args.output_model_path)
+
+seg_feed_size=args.feedforward_size // args.tp_size
+seg_hidden_size = args.hidden_size // args.tp_size
+seg_word_size=input_model["embedding.word.embedding.weight"].size()[0] // args.tp_size
+
+for n in range(args.tp_size):
+    model_piece=collections.OrderedDict()
+    seg_dim=input_model["embedding.word.embedding.weight"].size()[0]//args.tp_size
+    model_piece["embedding.word.embedding.weight"] = input_model["embedding.word.embedding.weight"][n* seg_dim :(n+1) * seg_dim,:]
+    
+    for i in range(args.layers_num):
+        for j in range(3):
+            model_piece["encoder.transformer." + str(i) + ".self_attn.linear_layers."+str(j)+".weight"] = input_model["encoder.transformer." + str(i) + ".self_attn.linear_layers."+str(j)+".weight"][n * seg_hidden_size:(n+1) * seg_hidden_size,:]
+        
+        model_piece["encoder.transformer." + str(i) + ".self_attn.final_linear.weight"] = \
+            input_model["encoder.transformer." + str(i) + ".self_attn.final_linear.weight"][:,n * seg_hidden_size:(n+1) * seg_hidden_size]
+        
+        model_piece["encoder.transformer." + str(i) + ".layer_norm_1.weight"] = \
+            input_model["encoder.transformer." + str(i) + ".layer_norm_1.weight"]
+       
+        model_piece["encoder.transformer." + str(i) + ".feed_forward.linear_1.weight"] = \
+            input_model["encoder.transformer." + str(i) + ".feed_forward.linear_1.weight"][n * seg_feed_size:(n+1)  * seg_feed_size,:]
+        
+        model_piece["encoder.transformer." + str(i) + ".feed_forward.linear_gate.weight"]= \
+            input_model["encoder.transformer." + str(i) + ".feed_forward.linear_gate.weight"][n * seg_feed_size:(n+1)  * seg_feed_size,:]
+        
+        model_piece["encoder.transformer." + str(i) + ".feed_forward.linear_2.weight"] = \
+            input_model["encoder.transformer." + str(i) + ".feed_forward.linear_2.weight"][:,n * seg_feed_size:(n+1)  * seg_feed_size]
+        
+        model_piece["encoder.transformer." + str(i) + ".layer_norm_2.weight"] = \
+            input_model["encoder.transformer." + str(i) + ".layer_norm_2.weight"]
+    
+    model_piece["encoder.layer_norm.weight"] = input_model["encoder.layer_norm.weight"]
+    
+    model_piece["target.lm.output_layer.weight"]= input_model["target.lm.output_layer.weight"][n * seg_word_size:(n+1) * seg_word_size,:]
+    
+    name=str(n) if len(str(n))==2 else '0'+str(n)
+    torch.save(model_piece, os.path.join(args.output_model_path,"mp_rank_"+str(name)+"_model_states.pt"))
+ 
